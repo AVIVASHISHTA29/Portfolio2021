d03931e3b9f1822d91a66255fb6241cae68589b7
Avi Vashishta <avivashishta@gmail.com>
2023-12-13 16:17:36 +0530

    macbook

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,40 @@
-import React from 'react'
-import styles from "./styles.module.css"
+import React from 'react';
+import { FiInstagram } from "react-icons/fi";
+import { Macbook3D } from '../../components/Home/Macbook3D';
+import styles from "./styles.module.css";
+
 function Home() {
     return (
-        <div className={styles.main}>Home</div>
+        <div className={styles.main}>
+            <div className={styles.upperDiv}>
+                <div className={styles.text}>
+                    <div className={styles.left}>
+                        <h1 className={styles.heading}>
+                            AVI
+                            <br />
+                            VASHISHTA.
+                        </h1>
+                        <h1 className={styles.strokeHeading}>
+                            WEB <br />
+                            DEVELOPER
+                        </h1>
+                    </div>
+                    <div className={styles.right}>
+                        <div className={styles.circle}>
+                            <FiInstagram />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.webProjects}>
+                    <h1 className={styles.webHeading}>
+                        WEB PROJECTS
+                    </h1>
+                    <div className={styles.model}><Macbook3D /></div>
+
+                </div>
+            </div>
+            <div className={styles.lowerDiv}></div>
+        </div>
     )
 }
 
